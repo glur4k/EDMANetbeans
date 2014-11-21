318593240b53836568955ef991fbec99e973a1f5
Glur4k <sandro@Sandros-MacBook-Air.local>
2014-11-21 17:42:57 +0100

    init.php
    Init-File fertig

--- core/init.php
+++ core/init.php
@@ -17,3 +17,16 @@ $GLOBALS['config'] = array(
     )
 );
 
+/**
+ * Lädt eine Klasse automatisch beim Aufruf $class = new Class();
+ * Somit muss nicht ständig ein require auf die benötigte Klasse gemacht werden.
+ * Stattdessen wird sie nur required, wenn sie auch tatsächlich benutzt wird.
+ */
+spl_autoload_register(function($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+/**
+ * Kann leider nicht so wie oben gemacht werden, da functions keine Klasse ist.
+ */
+require_once 'functions/sanitize.php';
