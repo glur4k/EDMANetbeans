542e2fc26fc0e6d1565dba76abc5203aab350cfb
Glur4k <tonon.san@gmail.com>
2014-11-22 11:51:01 +0100

    Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -6,5 +6,18 @@
  * @author Sandro
  */
 class Config {
-    //put your code here
+    public static function get($path = null) {
+        if ($path) {
+            $config = $GLOBALS['config']; 
+            $path = explode('/', $path);
+            
+            foreach ($path as $bit) {
+                if (isset($config[$bit])) {
+                    $config = $config[$bit];
+                }
+            }
+            
+            return $config;
+        }
+    }
 }
